1b63e082a89b9af5324f68fbfe34b41d7ce3e110
Benjamin Babik <work@benjaminbabik.com>
2025-08-14 08:37:12 +0000

    eg

--- Sources/MetaSwiftClient/main.swift
+++ Sources/MetaSwiftClient/main.swift
@@ -37,10 +37,26 @@ struct Abc {
     }
 }
 
+@trait()
+struct Xyz {
+    let xyz: Int
+    init() {
+        xyz = 99
+    }
+}
+
 @with(Abc.Trait)
 struct SomethingThatHasAbc {
     init(){
         self.abc.myMethod()
+    }
+}
+
+@with(Abc.Trait, Xyz.Trait)
+struct SomethingThatHasAbcAndXyz {
+    init(){
+        self.abc.myMethod()
+        print("xyz = \(self.xyz.xyz)")
     }
 }
 
@@ -49,4 +65,16 @@ if let abcFromSomethingWithAbc = try? Abc(from: somethingThatHasAbc) {
     print("somethingThatHasAbc conforms to WithTrait, somethingThatHasAbc has property abc, abc.abc = \(abcFromSomethingWithAbc.abc)")
 } else {
     print("somethingThatHasAbc does NOT conform to WithTrait")
+}
+
+let somethingThatHasAbcAndXyz = SomethingThatHasAbcAndXyz()
+if let abcFromSomethingWithAbcAndXyz = try? Abc(from: somethingThatHasAbcAndXyz) {
+    print("somethingThatHasAbcAndXyz conforms to WithTrait, somethingThatHasAbcAndXyz has property abc, abc.abc = \(abcFromSomethingWithAbcAndXyz.abc)")
+} else {
+    print("somethingThatHasAbcAndXyz does NOT conform to WithTrait")
+}
+if let xyzFromSomethingWithAbcAndXyz = try? Xyz(from: somethingThatHasAbcAndXyz) {
+    print("somethingThatHasAbcAndXyz conforms to WithTrait, somethingThatHasAbcAndXyz has property xyz, xyz.xyz = \(xyzFromSomethingWithAbcAndXyz.xyz)")
+} else {
+    print("somethingThatHasAbcAndXyz does NOT conform to WithTrait")
 }
